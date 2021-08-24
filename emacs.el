ef332cb0070a47906a9a885ec21894e99dce7138
rn7s2 <a3788670@foxmail.com>
2021-08-24 22:21:13 +0800

    update emacs config

--- emacs.el
+++ emacs.el
@@ -3,21 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier Prime" :foundry "QUQA" :slant normal :weight normal :height 141 :width normal)))))
+ '(default ((t (:family "Courier Prime" :foundry "QUQA" :slant normal :weight normal :height 163 :width normal)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(column-number-mode t)
  '(current-language-environment "UTF-8")
- '(custom-enabled-themes '(misterioso))
+ '(custom-enabled-themes '(adwaita))
  '(display-battery-mode t)
  '(display-time-mode t)
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(company company-dcd d-mode slime markdown-mode))
- '(show-paren-mode t))
+ '(package-selected-packages '(company company-dcd d-mode slime markdown-mode))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 
 (load "~/emacs-config/autoloads.el")
