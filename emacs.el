cf4c0c895120f26dcd04c4a5cb97a160e98e500c
rn7s2 <a3788670@foxmail.com>
2021-10-02 01:30:11 +0800

    update emacs config

--- emacs.el
+++ emacs.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier Prime" :foundry "QUQA" :slant normal :weight normal :height 163 :width normal)))))
+ '(default ((t (:family "Courier Prime" :foundry "QUQA" :slant normal :weight normal :height 141 :width normal)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
