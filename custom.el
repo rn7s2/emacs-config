fb42eb46cfc6fbacc4ce04f3ed82ba8c0da6bb2b
rn7s2 <a3788670@foxmail.com>
2021-12-07 19:01:02 +0800

    update emacs config

--- custom.el
+++ custom.el
@@ -1,15 +1,28 @@
+(exec-path-from-shell-initialize)
+
 (setq backup-directory-alist '(("." . "~/.emacs_backups")))
+
 (setq initial-scratch-message (insert-file-contents "~/emacs-config/note.txt"))
+
+(add-hook 'window-setup-hook 'toggle-frame-maximized)
+
 (setq-default fill-column 80)
 (global-display-fill-column-indicator-mode)
-(add-hook 'window-setup-hook 'toggle-frame-maximized)
+
+(global-company-mode)
+
 (add-hook 'prog-mode-hook 'hs-minor-mode)
-(setq c-basic-offset 4)
-(setq default-tab-width 4)
-(setq c-default-style "bsd")
-(setq-default indent-tabs-mode nil)
 (add-hook 'prog-mode-hook (lambda () (local-set-key (kbd "<f5>") 'quickrun-shell)))
+
+(setq default-tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+(setq c-basic-offset 4)
+(setq c-default-style "bsd")
+
 (setq inferior-lisp-program "sbcl")
-(setq rust-format-on-save t)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(setq-default line-spacing 0.27)
+
+(setq rust-format-on-save t)
+(add-hook 'rust-mode-hook #'lsp)
+(add-hook 'rust-mode-hook (lambda () (define-key rust-mode-map (kbd "C-c C-c") 'rust-run)))
