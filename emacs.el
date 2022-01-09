c454938c997fed70dee608680df7919a50be473d
rn7s2 <a3788670@foxmail.com>
2022-01-09 22:04:55 +0800

    update emacs config

--- emacs.el
+++ emacs.el
@@ -29,7 +29,7 @@
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(cnfonts company exec-path-from-shell geiser-mit markdown-mode quickrun rainbow-delimiters rust-mode slime))
+   '(yaml-mode dart-mode cnfonts company exec-path-from-shell geiser-mit markdown-mode quickrun rainbow-delimiters rust-mode slime))
  '(rainbow-delimiters-max-face-count 6)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
