1f920f15bb2e1165b6a263aa3509d0982b76032e
rn7s2 <mail@rn7s2.cn>
2023-04-05 18:23:23 +0800

    Chez Scheme.

--- custom.el
+++ custom.el
@@ -59,5 +59,3 @@ apps are not started from a shell."
                                    scheme-mode-hook
                                    emacs-lisp-mode-hook)
                                  'rainbow-delimiters-mode)
-(setq inferior-lisp-program "sbcl")
-(slime-setup '(slime-fancy slime-company slime-quicklisp slime-asdf))
--- emacs.el
+++ emacs.el
@@ -39,7 +39,7 @@
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(cnfonts company rainbow-delimiters slime slime-company smart-mode-line smart-mode-line-powerline-theme))
+   '(cnfonts company rainbow-delimiters geiser-chez smart-mode-line smart-mode-line-powerline-theme))
  '(rainbow-delimiters-max-face-count 6)
  '(slime-company-completion 'fuzzy)
  '(tool-bar-mode nil))
